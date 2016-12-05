02b86b0651ebd0f1a49ab0290841f82fbfb8db35
melikfinneg <aesoukourian@finnegans.com.ar>
2016-12-05 09:48:57 -0300

    Create sysQuerys.sql

---
+++ sysQuerys.sql
@@ -0,0 +1,22 @@
+--para ver qué tabla contiene la columna buscada
+SELECT 
+c.name AS ColName, 
+t.name AS TableName
+FROM sys.columns c
+JOIN sys.tables t ON c.object_id = t.object_id
+WHERE c.name LIKE '%TablaDimensionID'
+
+--para buscar tablas
+SELECT 
+t.name AS TableName
+FROM sys.tables t
+WHERE 
+t.name LIKE '%dimension%'
+
+--para ver qué columnas contiene la tabla buscada
+SELECT 
+c.name AS ColName, 
+t.name AS TableName
+FROM sys.columns c
+JOIN sys.tables t ON c.object_id = t.object_id
+WHERE t.name LIKE '%BSTablaDimension%'
